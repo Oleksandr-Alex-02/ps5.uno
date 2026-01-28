5b9f9da14957a962b9b6a7886223bd4608415545
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2026-01-28 23:25:00 +0200

    Hero

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,17 +1,32 @@
+'use client';
+
+
 import Link from "next/link";
+import css from './Header.module.css'
+
+import { useEffect, useState } from "react";
 
 const Navigation = () => {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrolled(window.scrollY > 50); // якщо прокрутка більше 50px
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
 
     return (
-        <section>
-            <nav>
-                <p>Оренда техніки у Києві +380958202612</p>
-                <ul>
+        <section className={`${css.navigation} ${scrolled ? css.scrolled : ""}`}>
+            <nav className={css.title}>
+                <p className={css.pp}>Оренда техніки у Києві +380958202612</p>
+                <ul className={css.ul}>
                     <li><Link href='/'>Оренда ПС5</Link></li>
                     <li><Link href='/generator'>Оренда генераторів</Link></li>
                     <li><Link href='/ninebot'>Оренда електросамокатів</Link></li>
                     <li><Link href='/interesting-articles'>Місцезнаходження</Link></li>
-                    <li>+380958202612</li>
+                    <li><a href="tel:+380958202612">+380958202612</a></li>
                 </ul>
             </nav>
         </section>
