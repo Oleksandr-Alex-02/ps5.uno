796bba91c0c6a6d42e639946dbc54eb11b2bb03b
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2026-02-07 17:13:38 +0200

    Ninebot

--- app/ninebot/page.tsx
+++ app/ninebot/page.tsx
@@ -1,13 +1,33 @@
-import InDevelopment from '@/components/InDevelopment/InDevelopment'
+
+import Social from '@/components/Social/Social';
+import css from './ninebot.module.css'
+import Delivery from '@/components/Delivery/Delivery';
+import { KharkivHighway, Bakunska } from '@/components/Address/Address';
+import Phone from '@/components/Phone/Phone';
 
 
 const Ninebot = () => {
-    const isDevelopment = true;
-    // const isDevelopment = false;
 
     return (
         <section>
-            {isDevelopment ? <InDevelopment /> : ""}
+            <section className={css.container_generator}>
+                <div className={css.hero}>
+                    <h1 className={css.hero_title}>Оренда електросамокатів</h1>
+                    <a className={css.hero_button} href="tel:+380958202612">Зателефонувати +380958202612</a>
+                </div>
+            </section>
+            <Social />
+
+            <Delivery />
+
+            <section id="pickup_location" className={css.rent_block}>
+                <ul className={css.address}>
+                    <KharkivHighway />
+                    <Bakunska />
+                </ul>
+            </section>
+
+            <Phone />
         </section>
     );
 };
