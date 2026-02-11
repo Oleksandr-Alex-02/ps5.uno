d9808e51a83cf5378bd3abd3259581a8b4cee8ca
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2026-02-11 17:13:03 +0200

    Games

--- app/includedPS5/page.tsx
+++ app/includedPS5/page.tsx
@@ -1,13 +1,25 @@
+
+import css from './includedPS5.module.css'
+
 import Games from "@/components/Games/Games"
-import Hero from "@/components/Hero/Hero";
+import Phone from "@/components/Phone/Phone";
+import Social from "@/components/Social/Social";
 
 
 const Included = () => {
 
     return (
         <section>
-            <Hero />
+            <section className={css.container_hero}>
+                <div className={css.hero}>
+                    <h1 className={css.hero_title}>Що входить в комплект при оренді PlayStation?</h1>
+                    <a className={css.hero_button} href="tel:+380958202612">Зателефонувати +380958202612</a>
+                </div>
+            </section>
+            <Social />
             <Games />
+            <Phone />
+            <Social />
         </section>
     )
 };
